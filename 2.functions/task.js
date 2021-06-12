bcc0219c1a443051fa0b318e547b6476c6a51722
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-06-12 17:51:29 +0300

    task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,51 +1,39 @@
 // Задание 1
 function getArrayParams(arr) {
-  var max = arr[0]; 
-  var min = arr[0];
-  var sum = 0; // sum - сумма всех элементов массива
-  
-
-    for (var i = 0; i < arr.length; i++) {
-		if (Object.prototype.toString.call(arr[i])) {
-			sum += arr[i];
-		}
-    avg = sum / arr.length;
-    if (arr[i] < min) min = arr[i];
-    if (max < arr[i]) max = arr[i];
-  }
+  let max = arr[0]; 
+  let min = arr[0];
+  let sum = 0; 
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+      if (min >arr[i]) min = arr[i];
+      if (max < arr[i]) max = arr[i];
+      avg= sum / arr.length;
+     avg = avg.toFixed(2); 
+    }
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
-function worker(arr){
-  var sum = 0;
-  for (var i = 0; i < arrOfArr.length; i++) {
-    var sum = 0;
-    for (var k = 0; k < arrOfArr[i].length; k++) {
-    sum += arrOfArr[i][k];
-    };
-    console.log('сумма подмассива ' + i + ' равна ' + sum);
-    sum += sum;
-    };
-    console.log('общая сумма равна ' + sum);
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   // Ваш код
-
-  return sum;
+ return sum;
 }
-
 function makeWork(arrOfArr, func) {
-  let max = -100000000000;
-  for (let i = 0; i < arrOfArr.length; i++){
-    func(...arrOfArr[i])
-    console.log(sum);
-     if (sum > max){
-       max = sum;
-     }
-   }
-  // Ваш кода
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+worker(arrOfArr);
+if (max < worker(arrOfArr[i])) {
+max = worker(arrOfArr[i]);
+}
+}
+ // Ваш кода
   // for ...
   
-  return max
+return max
 }
 
 // Задание 3
